63026d21f2e80cd2aa089ff33424dec70209c265
gonglixue <724351282@qq.com>
2016-12-26 19:29:46 +0800

    index component

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by chenjun on 2016/12/26.
+ */
+module.exports = {
+    template: require('../templates/index.html'),
+    ready:function(){
+
+    }
+};
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+module.exports = {
+    '/':{
+        component: require('./components/index')
+    },
+    '/list':{
+        component: require('./components/list')
+    },
+    '*':{
+        component: require('./components/notFound')
+    }
+
+}
