1f5cccf29c4ab918591d80c11dc5bd4c824faeb8
gonglixue <724351282@qq.com>
2016-12-26 20:29:04 +0800

    template list

--- src/components/list.js
+++ src/components/list.js
@@ -1,3 +1,15 @@
 /**
  * Created by chenjun on 2016/12/26.
  */
+module.exports={
+    template:require('../templates/list.html'),
+    data:function(){
+        return {
+            items:[
+                {"id":1, "name":"hello1"},
+                {"id":2, "name":"hello2"}
+            ]
+        }
+    },
+    ready:function(){}
+}
